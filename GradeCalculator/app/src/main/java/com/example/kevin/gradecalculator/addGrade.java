6de347b241fda9dd01e315dde85076eea5765fe6
Robert Cole <Rob.eric.cole@gmail.com>
2015-11-22 20:18:32 -0500

    Base to add grade
    added base parts to add grade

--- GradeCalculator/app/src/main/java/com/example/kevin/gradecalculator/addGrade.java
+++ GradeCalculator/app/src/main/java/com/example/kevin/gradecalculator/addGrade.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class AddGrade extends AppCompatActivity {
 
@@ -28,5 +29,9 @@ public class AddGrade extends AppCompatActivity {
 
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void addGrade(View view) {
+        //TODO return to showGrade
     }
 }
